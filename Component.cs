a590e82fb8409a0bcc78e9cffa4377af5a04cfab
DentedGlassJar <nathanielwhite867991@gmail.com>
2025-02-24 09:58:18 -0400

    Classes Begun

---
+++ Component.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityComponent_NathanielWhite_20250224
+{
+    public class Component
+    {
+
+    }
+
+    public class RenderingComponent : Component
+    {
+
+    }
+
+    public class InputComponent : Component
+    {
+
+    }
+}
---
+++ GameObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityComponent_NathanielWhite_20250224
+{
+    public class GameObject
+    {
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
